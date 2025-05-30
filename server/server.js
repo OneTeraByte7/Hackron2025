54541e204c2ef8104dfd5c7a7b8bbfd6b8865a71
Soham Suryawanshi <119783385+OneTeraByte7@users.noreply.github.com>
2025-05-30 21:16:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config(); // For environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 const __filename = fileURLToPath(import.meta.url);
