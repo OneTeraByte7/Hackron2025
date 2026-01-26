72cbb947360f6a3ba6c5ddb68ce3e5bb73e103f2
Soham Suryawanshi <suryawanshisoham7@gmail.com>
2026-01-26 17:43:56 +0530

    deployed:done

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -57,7 +57,7 @@ const MapWithWasteChart = () => {
 
       mapRef.current = map;
     }
-  }, [updateChartData]);
+  }, [updateChartData, darkStores]);
 
   // Initialize chart
   useEffect(() => {
